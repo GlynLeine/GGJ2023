e6b44b981a5090f17dac1155927c39575bc5e41f
Rowan Ramsey <blazinram@gmail.com>
2023-02-03 23:40:02 +0100

    cleaning up main

--- Tiled/conveyorbeltsprite.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.3" name="conveyorbeltsprite" tilewidth="64" tileheight="64" tilecount="3" columns="3">
- <image source="conveyorbeltsprite.png" width="192" height="64"/>
-</tileset>
