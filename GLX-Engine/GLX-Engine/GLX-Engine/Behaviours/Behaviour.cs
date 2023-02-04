9b35e5ded4e28c8bee2d4527cd95bf974ac97918
Glyn Leine <g.m.leine@gmail.com>
2023-02-04 01:53:00 +0100

    behaviours and a new gameobject

---
+++ GLX-Engine/GLX-Engine/GLX-Engine/Behaviours/Behaviour.cs
@@ -0,0 +1,20 @@
+﻿using GLXEngine;
+using GLXEngine.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GLX_Engine
+{
+    public abstract class Behaviour
+    {
+        private GameObject m_gameObject;
+
+        public GameObject gameObject { get { return m_gameObject; } }
+
+        public abstract void Update();
+        public virtual void Render(GLContext glContext) { }
+    }
+}
